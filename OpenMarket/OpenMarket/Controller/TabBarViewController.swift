41bf9c4b2cdd0353cb7028d7ea642b1e62c5fb28
Jager-yoo <y73447jh@gmail.com>
2022-01-13 03:20:27 +0900

    feat: Segment Control 변경을 확인하는 메서드 구현
    - Navigation Bar 오른쪽에 Add 버튼 추가

--- OpenMarket/OpenMarket/Controller/TabBarViewController.swift
+++ OpenMarket/OpenMarket/Controller/TabBarViewController.swift
@@ -8,7 +8,21 @@ import UIKit
 
 class TabBarViewController: UITabBarController {
 
-    @IBOutlet weak var segmentSwitch: UISegmentedControl!
+    @IBOutlet private weak var segmentSwitch: UISegmentedControl!
+    @IBOutlet private weak var hiddenTabBar: UITabBar!
+    
+    @IBAction func switchViewLayout(_ sender: UISegmentedControl) {
+        // 이미 해당 화면에 있을 경우, 요청을 무시하는 로직을 넣지 않아도, 다시 호출되지 않는 듯?
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("🧡 LIST 선택됨!")
+            // 어떻게 해당 탭바 아이템이 클릭되는 효과를 줄 수 있을까?
+        case 1:
+            print("💚 GRID 선택됨!")
+        default:
+            fatalError()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
